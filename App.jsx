ebc87887189f3b0bcb516f265b94c782c6a41dbe
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-06-09 11:29:03 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./component/Header";
+import Header from "./component/Header"; 
 import "./App.css";
 import Sidebar from "./component/Sidebar";
 import Footer from "./component/Footer";
